86a5c88ee7d41482415727097780dca385c49745
Zayn <zayn.shah1@gmail.com>
2024-10-11 17:04:57 -0700

    Error update

--- TwoSum/src/main.rs
+++ TwoSum/src/main.rs
@@ -1,19 +1,28 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+// use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+//         let mut map = HashMap::new();
+//         for (i, &value) in nums.iter().enumerate() {
+//             let complement = target - value;
+//             if map.contains_key(&complement) {
+//                 return vec![*map.get(&complement).unwrap() as i32, i as i32];
+//             }
+//             map.insert(value, i);
+//         }
+//         vec![]
+//     }
+// }
+
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-}
-
-use std::collections::HashMap;
-
-impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-        for (i, &value) in nums.iter().enumerate() {
-            let complement = target - value;
-            if map.contains_key(&complement) {
-                return vec![*map.get(&complement).unwrap() as i32, i as i32];
-            }
-            map.insert(value, i);
-        }
-        vec![]
-    }
+    // Try running with a different env var - `PORT=4000 cargo run`
+    let port = env::var("PORT").unwrap_or("3000".to_string());
+    println!("{}", port);
 }
